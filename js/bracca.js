c1a32849183187d1cbf9f562d04f7c54fdfa6854
CE PH <generalskaar12@gmail.com>
2023-04-03 11:49:23 +0100

    Added bracca mission. Will move to correct spot

--- js/bracca.js
+++ js/bracca.js
@@ -456,6 +456,11 @@ Vue.component('mission', {
                 name: 'oFinn',
                 toons: ['finn_o', 'rey_(jedi_training)', 'poe_dameron', 'bb-8', 'r2-d2']
              },
+             {
+                id:81,
+                name: 'Cere and Cal',
+                toons: ['cere_junda', 'cal_kestis', 'noone', 'noone', 'noone']
+             },
              
 
 
@@ -505,6 +510,7 @@ Vue.component('mission', {
         "Wave 2: Ninth Sister (L), 3x Purge Trooper, Stormtrooper",//40
         "Wave 2: Crosshair, 2x Purge Trooper, Stormtrooper, Stormtrooper Commando",//41
         "Wave 2: Second Sister, 3x Purge Trooper, Stormtrooper",//42
+        "Some combination of second sister and purge troopers - will update soon.",
 		],
 		platoons: [
         ["Razor Crest",	"12","Ben Solo",	"10","Commander Ahsoka Tano",	"5","Grand Master Yoda",	"4","BB-8",	"2","BTL-B Y-wing Starfighter",	"2","C-3PO",	"2",
@@ -556,7 +562,7 @@ Vue.component('mission', {
         'Light Side Ships (7-Star)',
         "5x Jedi (Relic 6+)",
         "5x Light Side or Neutral (Relic 6+)",
-        
+        "Cere Junda (Relic 7+), Any Cal Kestis(Relic 7+)" 
         ],
 		platims:[
         'Phase 2 LS Bracca (self)',
@@ -603,6 +609,19 @@ Vue.component('mission', {
 			dangerousEnemies: [39,41],
             reqs: [13],
             modifiers: [2,3],
+			},
+            {
+			id: 4,
+			name: 'c4',
+			type: 'special',
+			position: 'right',
+			rewards: [
+				'Wave 1: 50 GET2',
+				],
+            preferredSquads: [81],//generic
+			dangerousEnemies: [43],
+			modifiers: [2,3],
+            reqs: [14],
 			},
 			{
 			id: 5,
