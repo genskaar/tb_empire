2147f083f0e779883870fdcec10cd37cbf52556d
genskaar <generalskaar12@gmail.com>
2023-07-17 12:05:24 +0100

    video on felucia

--- js/felucia.js
+++ js/felucia.js
@@ -456,8 +456,9 @@ Vue.component('mission', {
              },
              {
 				id: 83,
-				name: 'Gas/Rey 501st',
-				toons: ['general_skywalker', 'ct-21-0408__echo_','ct-7567__rex_','arc_trooper','ct-5555__fives_',]
+				name: 'Rey/Gas 501st',
+				toons: ['gl_rey', 'ct-21-0408__echo_','ct-7567__rex_','arc_trooper','ct-5555__fives_',],
+				video: "https://www.youtube.com/watch?v=PaU-AA1Re3Q"
 				
 			 },
 			 {
@@ -607,7 +608,7 @@ Vue.component('mission', {
 				'Wave 1: 125,000',
                 'Wave 2: 250,000'
 				],
-            preferredSquads: [44,82,83,80],//generic
+            preferredSquads: [83,82,44,80],//generic
 			dangerousEnemies: [39,40],
             reqs: [12],
             notes: [3]
